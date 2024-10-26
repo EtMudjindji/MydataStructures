24f668ce95a58b731ab9e4b606128fa9577a252d
etienne mudjindji <etiennemudjindj@gmail.com>
2024-10-26 03:50:11 -0500

    Add a New Fille " MyStackArray
    In this file , i have developped another  methid of implementing  a stack by using a Array , with all the methid included. and Array is resisable whenever it is full

---
+++ Mystack/MyStackArray.cpp
@@ -0,0 +1,59 @@
+#ifndef MYSTACKARRAY_H
+#define MYSTACKARRAY_H
+
+using namespace std;
+#include <iostream>
+
+class MyArrayStack {
+    public:
+        int size;
+        int* Number;
+        int top;
+
+        MyArrayStack(int initialSize = 5) : size(initialSize), top(-1) {
+            Number = new int[size];
+        }
+
+        ~MyArrayStack() {
+            delete[] Number;
+        }
+
+        bool IsEmpty() {
+            return top == -1;
+        }
+
+        bool isFull() {
+            return top == size - 1; // Correct condition for full stack
+        }
+
+        void resize() {
+            int newSize = size * 2;
+            int* newArray = new int[newSize];
+            for (int i = 0; i < size; ++i) {
+                newArray[i] = Number[i];
+            }
+            delete[] Number;
+            Number = newArray;
+            size = newSize;
+            cout << "Stack resized to " << newSize << " elements.\n";
+        }
+
+        void push(int data) {
+            if (isFull()) {
+                resize();
+            }
+            Number[++top] = data;
+        }
+
+        void printStack() {
+            if (IsEmpty()) {
+                cout << "Stack is Empty\n";
+                return;
+            }
+            for (int i = 0; i <= top; ++i) {
+                cout << Number[i] << endl;
+            }
+        }
+};
+
+#endif
